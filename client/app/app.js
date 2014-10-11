c2d4b3a557bb9599be068f2c9dade1f66c521530
tgrrtt <jtgarrett123@gmail.com>
2014-10-10 19:40:55 -0700

    Css Styling  & HTML added :lipstick:
    made it look so pretty

--- client/app/app.js
+++ client/app/app.js
@@ -18,12 +18,10 @@ angular.module('shortly', [
     .when('/links', {
       templateUrl: 'app/links/links.html',
       controller: 'LinksController'
-      // authenticate: true
     })
     .when('/shorten', {
       templateUrl: 'app/shorten/shorten.html',
       controller: 'ShortenController'
-      // authenticate: true
     })
     .otherwise({redirectTo: '/links'});
 
